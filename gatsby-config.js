4359e4891dac8f6d2684311fc547854d1bf59946
Erik Eggers <em.eggers@gmail.com>
2018-10-08 02:06:33 -0400

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
     }, 
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify-cms',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
