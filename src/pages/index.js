35d430b80ca1f05bd5199def6b8cd631a13eff16
Erik <em.eggers@gmail.com>
2019-08-26 23:53:14 -0400

    saving

--- src/pages/index.js
+++ src/pages/index.js
@@ -89,7 +89,7 @@ const IndexPage = ({ data }) => (
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FInmanLLC%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=112677395506576"
         width="340"
         height="500"
-        style={{border:'none', overflow: 'hidden'}}
+        style={{ border: 'none', overflow: 'hidden' }}
         scrolling="no"
         frameBorder="0"
         allowtransparency="true"
