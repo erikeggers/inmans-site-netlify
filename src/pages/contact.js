e5f56f0b63a2ad8d5ec14f6f46cfa2530da15287
Erik <em.eggers@gmail.com>
2019-08-27 18:03:48 -0400

    added honeypot

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Layout from '../components/layout'
 // import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import ReCAPTCHA from 'react-google-recaptcha'
 
 // const LocationsPage = ({data}) => (
 const ContactPage = () => (
@@ -12,17 +11,22 @@ const ContactPage = () => (
         name="Contact Form"
         method="POST"
         data-netlify="true"
-        data-netlify-recaptcha="true"
+        netlify-honeypot="bot-field"
         action="/thank-you"
       >
         <input type="hidden" name="form-name" value="JSX Form" />
+     
+        <label className="hidden">
+          Don’t fill this out if you're human: <input name="bot-field" />
+        </label>
+   
         <label>Your Name:</label>
         <input type="text" name="name" />
         <label>Your Email:</label>
         <input type="email" name="email" />
         <label>
           Reason for contacting:{' '}
-          <select name="reason[]" multiple>
+          <select name="reason[]">
             <option value="returns">Returns</option>
             <option value="buy">Buy</option>
           </select>
@@ -30,7 +34,6 @@ const ContactPage = () => (
         <label>Message:</label>
         <textarea name="message" />
         <br />
-        <ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_KEY} />
         <button type="submit">Send</button>
       </form>
     </Wrapper>
@@ -40,6 +43,9 @@ const ContactPage = () => (
 export default ContactPage
 
 const Wrapper = styled.div`
+  .hidden {
+    display: none;
+  }
   max-width: 700px;
   margin: 0 auto;
   padding: 60px 8px 0 8px;
